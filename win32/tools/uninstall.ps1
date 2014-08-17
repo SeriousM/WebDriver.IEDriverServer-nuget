4098ad7e0ce641aaebbaacdf6f3ba99d43a9e7ed
jeremy simmons <jsimmons@jeremysimmons.net>
2014-08-17 16:00:00 -0400

    fix(install,uninstall): handle the case when Packages is in same folder as CSPROJ file

--- win32/tools/uninstall.ps1
+++ win32/tools/uninstall.ps1
@@ -1,5 +1,22 @@
 param($installPath, $toolsPath, $package, $project)
 
-$file = Join-Path (Join-Path $toolsPath '..\content') 'IEDriverServer.exe' | Get-ChildItem
+$driverFile = "IEDriverServer.exe"
+$contentPath = Join-Path $installPath "content"
+$driverPath = Join-Path $contentPath $driverFile
 
-$project.ProjectItems.Item($file.Name).Delete()
+# Treat the project file as a MSBuild script xml instead of DTEnv object model.
+Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+$projectXml = ([Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | select -First 1).Xml
+
+# Delete IEDriverServer content item
+$projectXml.Children | ` # All children of root <Project>
+Where-Object   { $_ -is [Microsoft.Build.Construction.ProjectItemGroupElement] } | ` # Elements that are <ItemGroup>
+ForEach-Object { $_.Children } | ` # Children of the ItemGroup elements
+Where-Object   { ($_.Children | Where-Object { $_.Name -eq "Link" -and $_.Value -eq $driverFile}) -ne $null } | ` # item trying to find
+ForEach-Object {
+    # Remove the item from the parent
+    $itemGrp = $_.Parent
+    $itemGrp.RemoveChild($_)
+    # remove the item group if it has no children af removing the item
+    if ($itemGrp.Children.Count -eq 0) { $projectXml.RemoveChild($itemGrp) }
+}
--- win64/tools/install.ps1
+++ win64/tools/install.ps1
@@ -1,11 +1,24 @@
 param($installPath, $toolsPath, $package, $project)
 
-$file = Join-Path (Join-Path $toolsPath '..\content') 'IEDriverServer.exe' | Get-ChildItem
+$driverFile = "IEDriverServer.exe"
+$contentPath = Join-Path $installPath "content" 
+$driverPath = Join-Path $contentPath $driverFile
+$nupkgName = Split-Path $installPath -Leaf
 
-# because the IEDriverServer.exe is copied to the project root, we have to remove it and add a file-link again
-$project.ProjectItems.Item($file.Name).Delete()
+# remote the automatically added IEDriverServer.exe from the {packageRoot}\Deploy
+$project.ProjectItems.Item($driverFile).Delete()
+$project.Save()
 
-$project.ProjectItems.AddFromFile($file.FullName);
-$pi = $project.ProjectItems.Item($file.Name);
-$pi.Properties.Item("BuildAction").Value = [int]2;
-$pi.Properties.Item("CopyToOutputDirectory").Value = [int]2;
+# add IEDriverServer.exe as a file link
+# use the MSBuild API to access the xml dom of the csproj file 
+Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+$projectXml = ([Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | select -First 1).Xml
+$projectUri = [uri]$project.FullName;
+$drivertUri = [uri]$driverPath;
+$driverRelativePath = $projectUri.MakeRelative($drivertUri) -replace "/","\"
+
+$itemGrp = $projectXml.CreateItemGroupElement()
+$projectXml.AppendChild($itemGrp)
+$item = $itemGrp.AddItem("Content", $driverRelativePath)
+$item.AddMetadata("Link", $driverFile)
+$item.AddMetadata("CopyToOutputDirectory", "PreserveNewest")
