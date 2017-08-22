42f83c2af5c9e1250efcd80db6c9f7a21f9e6237
Bernhard Millauer <bmillauer@intercom.at>
2017-08-22 14:05:43 +0200

    update api key url

--- build/push-win64.bat
+++ build/push-win64.bat
@@ -4,7 +4,7 @@ set PWD="%CD%"
 set NUGET=%PWD%\NuGet.exe
 set VERSION=win64
 
-set /P APIKEY=Enter your Api Key from https://www.nuget.org/account: 
+set /P APIKEY=Enter your Api Key from https://www.nuget.org/account/ApiKeys: 
 
 if "%APIKEY%"=="" GOTO ERROR
 
