412048b46cfac6b68584dd56e378ef9aab050a38
Bernhard Millauer <bmillauer@intercom.at>
2017-12-12 10:36:20 +0100

    update build scripts

--- build/build-win32.bat
+++ build/build-win32.bat
@@ -6,4 +6,4 @@ set VERSION=win32
 
 DEL %PWD%\..\%VERSION%\*.nupkg
 
-%NUGET% pack -Verbosity detailed -OutputDirectory %PWD%\..\%VERSION% -Exclude "build\*;.git\*;README.md" %PWD%\..\%VERSION%\WebDriver.IEDriverServer.%VERSION%.nuspec
+%NUGET% pack -Verbosity detailed -OutputDirectory %PWD%\..\%VERSION% -Exclude "**/build/**;**/.git/**;**/.git*;**/README.md" %PWD%\..\%VERSION%\WebDriver.IEDriverServer.%VERSION%.nuspec
